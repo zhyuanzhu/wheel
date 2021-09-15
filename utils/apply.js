d0bf0e4da52b6430990e44007d66ca51b3deab96
alphazhangyz <alphazhangyz@gmail.com>
2021-09-15 09:34:21 +0800

    fix apply

--- utils/apply.js
+++ utils/apply.js
@@ -2,7 +2,7 @@ Function.prototype._apply = function (thisArg, args) {
   thisArg = thisArg ? Object(thisArg) : window
   let result = null
   const temp = Symbol('apply')
-  thisArg[temp] = thisArg
+  thisArg[temp] = this
   if (args) {
     result = thisArg[temp](...args)
   } else {
